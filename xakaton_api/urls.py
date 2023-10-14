c19744f30823eb62c5abc93d74d85cbc0930f45c
Suxrob Ergashev <suxrobergashev2102@gmail.com>
2023-10-14 23:27:12 +0500

    add new router

--- xakaton_api/urls.py
+++ xakaton_api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework import routers
-from .views import CreateUser, EmergenciesViewSet, DiseaseStateViewSet, ComplainViewSet, get_by_list
+from .views import CreateUser, EmergenciesViewSet, DiseaseStateViewSet, ComplainViewSet, get_by_list, EmergenciesHistoryViewSet
 
 router = routers.DefaultRouter()
 router.register('user', CreateUser, "user")
 router.register('ttb', EmergenciesViewSet, " ttb")
 router.register('disease', DiseaseStateViewSet, "disease")
 router.register('complain', ComplainViewSet, "complain")
+router.register('history', EmergenciesHistoryViewSet, "history")
+
 
 urlpatterns = [
     path("ttb/<str:name>/", get_by_list)
