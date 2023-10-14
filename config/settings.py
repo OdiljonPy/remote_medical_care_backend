cba94a6a4cbc98b8af71967f0ad624431034e9d0
odiljon <odiljonabduvaitov@gmail.com>
2023-10-14 12:52:31 +0500

    changes for deploy

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rmc',
+        'USER': 'postgres',
+        'PASSWORD': 'JobSchedulerV3$',
+        'HOST': 'localhost',
+        # 'HOST': '168.119.110.233',
+        'PORT': '5432',
     }
 }
 
