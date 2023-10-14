362ca49dcb296be7500d3fc80d3b694f4f082124
Suxrob Ergashev <suxrobergashev2102@gmail.com>
2023-10-14 22:06:15 +0500

    model add EmergenciesHistory models

--- xakaton_api/models.py
+++ xakaton_api/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class UserModel(models.Model):
     name = models.CharField(max_length=150, null=True, blank=True)
     user_id = models.IntegerField(default=0)
+    language = models.CharField(max_length=2, default='uz')
     username = models.CharField(max_length=150, null=True, blank=True)
-    phone_number = models.CharField(max_length=12)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    age = models.CharField(max_length=20, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -65,3 +67,18 @@ class Complain(models.Model):
 
     def __str__(self):
         return str(self.user.user_id)
+
+
+class EmergenciesHistory(models.Model):
+    user = models.IntegerField(default=0)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    age = models.CharField(max_length=20, null=True, blank=True)
+    category = models.CharField(max_length=150, null=True, blank=True)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user}"
